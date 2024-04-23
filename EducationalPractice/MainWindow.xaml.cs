92f4792067e9b8d782708f10fb09515cf962e02b
hotkik <hotkik@mail.ru>
2024-04-23 19:59:06 +0300

    Add ZXing.comp Encoding and saving QRCode

--- EducationalPractice/MainWindow.xaml.cs
+++ EducationalPractice/MainWindow.xaml.cs
@@ -8,17 +8,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using System.Drawing;
+using ZXing.Windows.Compatibility;
+
 
 namespace EducationalPractice
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TButt_Click(object sender, RoutedEventArgs e)
+        {
+            string tStr = "test string to QR-Code";
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+            BitMatrix matrix = qrCodeWriter.encode(tStr, BarcodeFormat.QR_CODE, 300, 300);
+
+            BarcodeWriter barcode = new BarcodeWriter();
+            Bitmap bitmap = barcode.Write(matrix);
+
+            bitmap.Save("QR.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            img.Source = new BitmapImage(new Uri("bin\\Debug\\net8.0-windows\\QR.png", UriKind.Relative));
         }
     }
 }
